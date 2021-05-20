e31b158cde1cddc9dbb407d5caea49f123bbd69d
Amanda Hoesman-Foley <amanda.clinnie@gmail.com>
2021-05-20 12:53:46 -0700

    Added confusion matrix analysis for DT models

--- DecisionTree.r
+++ DecisionTree.r
@@ -5,11 +5,12 @@
 # quick inspection of data
 aug_train %>% head(10)
 names(aug_train)
-summary(aug_train)
+summary(aug_train$target)
 
 # load libraries
 library("rpart")
 library("rpart.plot")
+library("caret")
 
 # set seed for reproducable results
 set.seed(515)
@@ -28,6 +29,12 @@ summary(tree)
 # plot tree
 rpart.plot(tree)
 
+# get confusion matrix
+pred = predict(tree, type="class")
+table(pred)
+
+confusionMatrix(pred, aug_train$target)
+
 # Tree #2 ----------------------------------
 # train classification tree model
 # predict based on all attributes except city_development_index 
@@ -42,6 +49,12 @@ summary(tree2)
 
 # plot tree
 rpart.plot(tree2)
+
+# get confusion matrix
+pred2 = predict(tree2, type="class")
+table(pred2)
+
+confusionMatrix(pred2, aug_train$target)
 
 # Tree #3 ----------------------------------
 # train classification tree model
@@ -58,6 +71,12 @@ summary(tree3)
 # plot tree
 rpart.plot(tree3)
 
+# get confusion matrix
+pred3 = predict(tree3, type="class")
+table(pred3)
+
+confusionMatrix(pred3, aug_train$target)
+
 # Tree #4 ----------------------------------
 # train classification tree model
 # predict based on only city_development_index attribute (excluding target)
@@ -72,6 +91,12 @@ summary(tree4)
 # plot tree
 rpart.plot(tree4)
 
+# get confusion matrix
+pred4 = predict(tree4, type="class")
+table(pred4)
+
+confusionMatrix(pred4, aug_train$target)
+
 # Tree #5 ----------------------------------
 # train classification tree model
 # predict based on only company_size attribute (excluding target)
@@ -85,3 +110,9 @@ summary(tree5)
 
 # plot tree
 rpart.plot(tree5)
+
+# get confusion matrix
+pred5 = predict(tree5, type="class")
+table(pred5)
+
+confusionMatrix(pred5, aug_train$target)
